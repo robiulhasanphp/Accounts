389b0f621c617b59554789292760ec923d402430
Md Robiul Hasan <robiulhasan.php@gmail.com>
2026-04-06 15:09:02 +0200

    Update CompanyRootController.php

--- src/Controller/CompanyRootController.php
+++ src/Controller/CompanyRootController.php
@@ -1,47 +1,51 @@
 <?php
-	namespace App\Controller;
-	
-	class CompanyRootController extends AppController{
-		
-		public function index(){
-			$this->set('CompanyRoot', $this->CompanyRoot->find('all')->contain('CompanyInfo'));
-			
-			
-			
-		}
+declare(strict_types=1);
 
-		public function getBranches(){
-				return 'xyx';
-		}
-		
-		
-		
-		public function view($RT_ID){
-			$this->set('CompanyRoot', $this->CompanyRoot->find('all')->where(['RT_ID'=>$RT_ID]));
-		}
-		
-		
-		
-		 public function add(){
-			 
-			$CompanyRoot = $this->CompanyRoot->newEntity();
-			if ($this->request->is('post')) {
-				$CompanyRoot = $this->CompanyRoot->patchEntity($CompanyRoot, $this->request->data);
-				if ($this->CompanyRoot->save($CompanyRoot)) {
-					$this->Flash->success(__('Your CompanyRoot has been saved.'));
-					return $this->redirect(array('action' => 'index'));
-				}
-				$this->Flash->error(__('Unable to add your article.'));
-			}
-			$this->set('CompanyRoot', $CompanyRoot);
-	
-			/*$query=$this->CompanyInfo->CompanyRoot->find('list',['keyField' => 'RT_ID','valueField' => 'RT_NAME']);
-			$CompanyRoot = $query->toArray();
-			$this->set(compact('CompanyRoot'));*/
-		}
-		
-		
-			
-		
-	}
-?>
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class CompanyRootController extends AppController
+{
+    public function index(): void
+    {
+        $companyRoot = $this->CompanyRoot->find()
+            ->contain(['CompanyInfo'])
+            ->all();
+
+        $this->set(compact('companyRoot'));
+    }
+
+    public function view(int $id): void
+    {
+        $companyRoot = $this->CompanyRoot->get($id);
+        $this->set(compact('companyRoot'));
+    }
+
+    public function add()
+    {
+        $companyRoot = $this->CompanyRoot->newEmptyEntity();
+
+        if ($this->request->is('post')) {
+
+            $companyRoot = $this->CompanyRoot->patchEntity(
+                $companyRoot,
+                $this->request->getData()
+            );
+
+            if ($this->CompanyRoot->save($companyRoot)) {
+                $this->Flash->success('Saved successfully');
+                return $this->redirect(['action' => 'index']);
+            }
+
+            $this->Flash->error('Save failed');
+        }
+
+        $this->set(compact('companyRoot'));
+    }
+
+    public function getBranches()
+    {
+        return $this->response->withStringBody('xyz');
+    }
+}
